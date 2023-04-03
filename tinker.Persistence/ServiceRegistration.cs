b54cc13b588d1e6e275ca0d2e629369ad7875d67
Murat Şahin <murat.sahin.dev@gmail.com>
2023-04-03 16:41:52 +0300

    Repository registrations are done.

--- tinker.Persistence/ServiceRegistration.cs
+++ tinker.Persistence/ServiceRegistration.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using tinker.Application.Interfaces.Repositories;
 using tinker.Persistence.Configurations;
 using tinker.Persistence.Contexts;
+using tinker.Persistence.Repositories;
 
 namespace tinker.Persistence
 {
@@ -10,6 +12,11 @@ namespace tinker.Persistence
         public static void AddPersistenceServices(this IServiceCollection services)
         {
             services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase(ConfigManager.InMemoryConnectionString));
+            services.AddScoped<IAddressRepository, AddressRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<ISellerRepository, SellerRepository>();
         }
     }
 }
